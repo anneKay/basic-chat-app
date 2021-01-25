523521d9aafdc0aa4e4c256c10f72e1f0719541d
Kalu Nwanna <nwannauk@gmail.com>
2021-01-25 01:21:40 +0100

    use message template to display messages

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -3,7 +3,7 @@ class ChatroomController < ApplicationController
 
   def index
     @message = Message.new
-    @messages = Message.includes(:user).all
+    @messages = Message.includes(:user).custom_display
   end
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user, :foreign_key => :user_id
   validates :body, presence: true
+
+  scope :custom_display, -> { order(:created_at).last(20) }
 end
